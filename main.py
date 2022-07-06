4d1ea0337d162ff944802c7a4391c24f84b0cdb5
Mirza S Reza <mirzasreza@gmail.com>
2022-07-06 14:12:22 +0600

    fetch users and register user

--- main.py
+++ main.py
@@ -25,4 +25,12 @@ db: List[User] = [
 @app.get("/")
 async def root():
     return {"Hello": "World"}
-    
+
+@app.get("/api/v1/users")   
+async def fetch_users():
+    return db
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
